c620d85c41a00cc9ef64de0fa160fa566be47fc7
icellus <2283411628@qq.com>
2018-06-04 01:43:29 +0800

    update trie.php

--- keyword_match_trie.php
+++ keyword_match_trie.php
@@ -1,72 +1,81 @@
 <?php
 
-class Trie {
-    /**
-     * node struct
-     *
-     * node = array(
-     * val->word
-     * next->array(node)/null
-     * depth->int
-     * )
-     */
-    private $root = array(
-        'depth' => 0,
-        'next' => array(),
-    );
+class Trie
+{
+	/**
+	 * node struct
+	 * node = array(
+	 * val->word
+	 * next->array(node)/null
+	 * depth->int
+	 * )
+	 */
+	public $root = [
+		'depth'  => 0,
+		'next'   => [],
+		'value'  => '',
+		'is_end' => true,
+	];
 
-    private $matched = array();
+	public $matched = [];
 
-    public function append($keyword) {
-        $words = preg_split('/(?<!^)(?!$)/u', $keyword);
-        array_push($words, '`');
-        $this->insert($this->root, $words);
-    }
+	public function append ($keyword)
+	{
+		$words = preg_split('/(?<!^)(?!$)/u', $keyword);
+		$this->insert($this->root, $words);
+	}
 
-    public function match($str) {
-        $this->matched = array();
-        $words = preg_split('/(?<!^)(?!$)/u', $str);
+	public function match ($str)
+	{
+		$this->matched = [];
+		$words         = preg_split('/(?<!^)(?!$)/u', $str);
 
-        while (count($words) > 0) {
-            $matched = array();
-            $res = $this->query($this->root, $words, $matched);
-            if ($res) {
-                $this->matched[] = implode('', $matched);
-            }
-            array_shift($words);
-        }
+		while (count($words) > 0) {
+			$this->query($this->root, $words);
+			array_shift($words);
+		}
 
-        return $this->matched;
-    }
+		return $this->matched;
 
-    private function insert(&$node, $words) {
-        if (empty($words)) {
-            return;
-        }
-        $word = array_shift($words);
-        if (isset($node['next'][$word])) {
-            $this->insert($node['next'][$word], $words);
-        } else {
-            $tmp_node = array(
-                'depth' => $node['depth'] + 1,
-                'next' => array(),
-            );
-            $node['next'][$word] = $tmp_node;
-            $this->insert($node['next'][$word], $words);
-        }
-    }
+	}
 
-    private function query($node, $words, &$matched) {
-        $word = array_shift($words);
-        if (isset($node['next'][$word])) {
-            array_push($matched, $word);
-            if (isset($node['next'][$word]['next']['`'])) {
-                return true;
-            }
-            return $this->query($node['next'][$word], $words, $matched);
-        } else {
-            $matched = array();
-            return false;
-        }
-    }
+	private function insert (&$node, $words)
+	{
+		if (empty($words)) {
+			return;
+		}
+		$word = array_shift($words);
+		if (isset($node['next'][ $word ])) {
+			$this->insert($node['next'][ $word ], $words);
+		} else {
+			$tmp_node = [
+				'depth'  => $node['depth'] + 1,
+				'next'   => [],
+				'value'  => $node['value'] . $word,
+				'is_end' => count($words) === 0 ? true : false,
+			];
+
+			$node['next'][ $word ] = $tmp_node;
+			$this->insert($node['next'][ $word ], $words);
+		}
+	}
+
+	private function query ($node, $words)
+	{
+		$word = array_shift($words);
+
+		if (isset($node['next'][ $word ])) {
+			if (isset($node['next'][ $word ]['next']['`'])) {
+				array_push($this->matched, $node['next'][ $word ]['value']);
+				$this->query($node['next'][ $word ], $words);
+			}
+		}
+
+		return;
+	}
 }
+
+$trie = new Trie();
+$trie->append('广');
+$trie->append('广州');
+$trie->match('广州市');
